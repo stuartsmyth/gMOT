80893c647fc4f82f4ad7e89d3a3f63c9ef081d6e
stuartsmyth <40466017+stuartsmyth@users.noreply.github.com>
2019-02-19 13:45:03 +0000

    rewriiten for new grating

--- GMOTPlotter.m
+++ GMOTPlotter.m
@@ -15,17 +15,17 @@ bh = 16.6; % horizontal polarization
 
 %% Correct power data for background light
 ii = measurements(:,1)- bi;
-i0 = measurements(:,2)- b0;
+i10 = measurements(:,2)- b0;
+i20 = measurements(:,5)- b0;
+i30 = measurements(:,8)- b0;
 i11 = measurements(:,3)- b1;
 i12 = measurements(:,6)- b1;
 i13 = measurements(:,9)- b1;
-iv1 = measurements(:,4)- bv;
-iv2 = measurements(:,7)- bv;
-iv3 = measurements(:,10)- bv;
-ih1 = measurements(:,5)- bh;
-ih2 = measurements(:,8)- bh;
-ih3 = measurements(:,11)- bh;
-I = [ii i0 i11 i12 i13 iv1 iv2 iv3 ih1 ih2 ih3]; 
+ip1 = measurements(:,4)- bv;
+ip2 = measurements(:,7)- bv;
+ip3 = measurements(:,10)- bv;
+
+I = [ii i10 i20 i30 i11 i12 i13 ip1 ip2 ip3]; 
 
 %% use function
 global N
@@ -33,38 +33,28 @@ N = 3; % number of grating faces
 [ eff ] = gmot( I ); % out is matrix eff that has all calculated values
 
 %% plotting
-x1 = [0.9 1 1.1]; % matrix of x values for plots
+x1 = [1 2 3]; % matrix of x values for plots
 x2 = [1.9 2 2.1];
 x3 = [2.9 3 3.1];
 
 
 figure (1)
-stem(eff(:,1))
-axis([0 4 0 100])
-ylabel('\eta_{0}(%)')
+bar(x1,eff(1:3))
+ylabel('\eta_{0}')
 xlabel('grating')
 
 figure (2)
-stem(x1,eff(1,2:4))
-hold on
-stem(x2,eff(2,2:4))
-stem(x3,eff(3,2:4))
-ylim([0 100])
-ylabel('\eta_{1}(%)')
+bar(x1,eff(4:6))
+ylabel('\eta_{1}')
 xlabel('grating')
-hold off
 
 figure(3)
-stem(eff(:,5))
-axis([0 4 0 100])
-ylabel('\eta_{b}(%)')
+bar (eff(7:9))
+ylabel('\eta_{b}')
 xlabel('grating')
 
 figure (4)
-stem(x1,eff(1,6:8))
-hold on
-stem(x2,eff(2,6:8))
-stem(x3,eff(3,6:8))
-ylabel('\epsilon_{p}(%)')
+bar(x1,eff(10:12))
+ylabel('\epsilon_{p}')
 xlabel('grating')
-hold off
+
--- gmot.m
+++ gmot.m
@@ -2,15 +2,20 @@ function [ eff ] = gmot( I )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 global N
-n0 = (I(:,2)./I(:,1).*100); %0th order efficeincy
-n11 = (I(:,3)./I(:,1).*100); %1st order efficiency face 1
-n12 = (I(:,4)./I(:,1).*100); %1st order efficiency face 2
-n13 = (I(:,5)./I(:,1).*100); %1st order efficiency face 3
-n1avg = (n11+n12+n13)./3;%average n1
-%nb =  (N.*n1avg)./(100-n0);  % radition balance non percentage
-nb =  (1./((N.*n1avg)./(100-n0)).*100);  % radition balance
-ep1 = (I(:,9)./I(:,1).*100); % polarisaition efficiency face 1
-ep2 = (I(:,10)./I(:,1).*100); % polarisaition efficiency face 2
-ep3 = (I(:,11)./I(:,1).*100); % polarisaition efficiency face 3
-eff = [n0(2:end) n11(2:end) n12(2:end) n13(2:end) nb(2:end) ep1(2:end) ep2(2:end) ep3(2:end)];
+n10 = I(:,2)./I(:,1); %0th order efficeincy face 1
+n20 = I(:,3)./I(:,1); %0th order efficeincy face 2
+n30 = I(:,4)./I(:,1); %0th order efficeincy face 3
+n11 = I(:,5)./I(:,1); %1st order efficiency face 1
+n12 = I(:,6)./I(:,1); %1st order efficiency face 2
+n13 = I(:,7)./I(:,1); %1st order efficiency face 3
+
+nb1 =  (N.*n11)./(1-n10);  % radition balance face 1
+nb2 =  (N.*n12)./(1-n20);  % radition balance face 1
+nb3 =  (N.*n13)./(1-n30);  % radition balance face 1
+
+ep1 = I(:,8)./I(:,5); % polarisaition efficiency face 1
+ep2 = I(:,9)./I(:,6); % polarisaition efficiency face 2
+ep3 = I(:,10)./I(:,7); % polarisaition efficiency face 3
+
+eff = [n10(2:end) n20(2:end) n30(2:end) n11(2:end) n12(2:end) n13(2:end) nb1(2:end) nb2(2:end) nb3(2:end) ep1(2:end) ep2(2:end) ep3(2:end)];
 end
