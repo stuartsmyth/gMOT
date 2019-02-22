06dbbe16ad0264daf57b81f6fb78bea321c1b1a4
stuartsmyth <40466017+stuartsmyth@users.noreply.github.com>
2019-02-22 14:05:40 +0000

    after retaking of data

--- GMOTPlotter.m
+++ GMOTPlotter.m
@@ -1,6 +1,6 @@
 % GMOT efficeinty plotter
 % Author stuart smyth
-% Last mod 3 FEB 19
+% Last mod 20 FEB 19
 
 %% Import power data from excel
 load ('Gratingmeasurements.mat');
@@ -30,31 +30,43 @@ I = [ii i10 i20 i30 i11 i12 i13 ip1 ip2 ip3];
 %% use function
 global N
 N = 3; % number of grating faces
-[ eff ] = gmot( I ); % out is matrix eff that has all calculated values
+[ eff ] = gmot( I );% out is matrix eff that has all calculated values
 
 %% plotting
-x1 = [1 2 3]; % matrix of x values for plots
+x1 = [0.9 1 1.1]; % matrix of x values for plots
 x2 = [1.9 2 2.1];
 x3 = [2.9 3 3.1];
 
 
 figure (1)
-bar(x1,eff(1:3))
+bar(x1,eff(1,1:3))
+hold on 
+bar(x2,eff(2,1:3))
+bar(x3,eff(3,1:3))
 ylabel('\eta_{0}')
 xlabel('grating')
 
 figure (2)
-bar(x1,eff(4:6))
+bar(x1,eff(1,4:6))
+hold on 
+bar(x2,eff(2,4:6))
+bar(x3,eff(3,4:6))
 ylabel('\eta_{1}')
 xlabel('grating')
 
 figure(3)
-bar (eff(7:9))
+bar(x1,eff(1,7:9))
+hold on 
+bar(x2,eff(2,7:9))
+bar(x3,eff(3,7:9))
 ylabel('\eta_{b}')
 xlabel('grating')
 
 figure (4)
-bar(x1,eff(10:12))
+bar(x1,eff(1,10:12))
+hold on 
+bar(x2,eff(2,10:12))
+bar(x3,eff(3,10:12))
 ylabel('\epsilon_{p}')
 xlabel('grating')
 
